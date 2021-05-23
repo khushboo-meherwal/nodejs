253f6109b926b1ddab8a9a62b7a0fb54889cbf60
khushboo-meherwal <83585250+khushboo-meherwal@users.noreply.github.com>
2021-05-23 17:16:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Welcome to NodeJs project-A good start!\n'
+  const msg = 'Hello node!\n'
   res.end(msg);
 });
 
